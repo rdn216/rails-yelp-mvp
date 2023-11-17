f4184456571c88d88dc9ea704382349cc380959b
Robert Nitschke <rdn216@outlook.com>
2023-11-17 16:50:40 +0800

    application with validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,10 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+  # validates :name, presence: true
+  validates :restaurant_id, presence: true
+  validates :content, presence: true
+  validates :rating, presence: true
+  validates :rating, inclusion: {in: (0..5).to_a, message: "%{value} is not a valid integer"}
+  validates :rating, numericality: { only_integer: true }
+  # validates :category, inclusion: { in: %w(chinese italian japanese french belgian, message: "%{value} is not a valid category")};
 end
